c46d91595e39606362afbf9ed62c2804f8609bb8
Venti1707 <nickohse3@gmail.com>
2024-04-07 11:32:56 +0800

    Update find-and-replace.js

--- Playlists Scripts/find-and-replace.js
+++ Playlists Scripts/find-and-replace.js
@@ -1,8 +1,9 @@
-// ! npm packages
+// npm packages
 const fs = require('fs');
 const c = require('colors');
+console.time(c.brightMagenta('Time taken'));
 
-// ! Replace function
+// Replace function
 function replace(inputFile, outputFile, replacements, callback) {
 
     fs.readFile(inputFile, 'utf8', (err, data) => {
@@ -24,7 +25,7 @@ function replace(inputFile, outputFile, replacements, callback) {
                 return;
             }
 
-            console.log(c.bold.brightGreen(`✅ ${outputFile} has been updated successfully.`));
+            console.log(c.brightGreen(`✅ Successfully updated ${outputFile}.`));
 
             if (typeof callback === 'function') {
                 callback();
@@ -33,7 +34,7 @@ function replace(inputFile, outputFile, replacements, callback) {
     });
 }
 
-// ! Duplicate function
+// Duplicate function
 function duplicate(sourceFile, destinationFile, callback) {
     fs.copyFile(sourceFile, destinationFile, err => {
         if (err) {
@@ -41,7 +42,7 @@ function duplicate(sourceFile, destinationFile, callback) {
             return;
         }
 
-        console.log(c.bold.brightGreen(`✅ ${sourceFile} contents duplicated to ${destinationFile} successfully.`));
+        console.log(c.brightGreen(`✅ Successfully duplicated contents in ${sourceFile} to ${destinationFile}.`));
 
         if (typeof callback === 'function') {
             callback();
@@ -49,17 +50,16 @@ function duplicate(sourceFile, destinationFile, callback) {
     });
 }
 
-// ! File paths
+// File paths
 const YTM = 'Music/YouTubeMusic.txt';
 const YT = 'Music/YouTube.txt';
 
-// ! Unwanted strings
+// Unwanted strings
 const BROWSE = 'browse/VL';
 const WWW = 'www.youtube.com';
-const MUSIC = 'music.youtube.com';
 const FS = '&feature=shared'
 
-// ! Find & replace objects
+// Find & replace objects
 const STN = {
     find: '&feature=shared',
     replace: ''
@@ -97,33 +97,35 @@ fs.readFile(YTM, 'utf8', (err, data) => {
             highlightedLine = highlightedLine.replace(FS, c.bold.brightYellow(FS));
             console.error(highlightedLine);
             console.log('');
-            console.info(c.bold.brightCyan(`ℹ Now executing replacing and duplicating functions`));
-            replace(YTM, YTM, BTP, () => {
-                console.log(c.bold.brightCyan(`✅ Successfully replaced any ${BTP.find} to ${BTP.replace} in ${YTM}`));
-                replace(YTM, YTM, WTM, () => {
-                    console.log(c.bold.brightCyan(`✅ Successfully replaced any ${WTM.find} to ${WTM.replace} in ${YTM}`));
-                    replace(YTM, YTM, STN, () => {
-                        console.log(c.bold.brightCyan(`✅ Successfully replaced any ${STN.find} to ${STN.replace} in ${YTM}`));
-                        duplicate(YTM, YT, () => {
-                            console.log(c.bold.brightCyan(`✅ Successfully duplicated ${YTM} to ${YT}`));
-                            replace(YT, YT, MTW, () => {
-                                console.log(c.bold.brightCyan(`✅ Successfully replaced any ${MTW.find} to ${MTW.replace} in ${YT}`));
-                            });
+            console.info(c.brightCyan(`ℹ Now executing replacing and duplicating functions`));
+            found = true;
+        }
+    });
+
+    if (!found) {
+        console.info(c.brightCyan(`ℹ No unwanted strings found in ${YTM}, duplicating contents to ${YT}`));
+        duplicate(YTM, YT, () => {
+            console.log(c.brightCyan(`ℹ Successfully replaced any ${MTW.find} to ${MTW.replace} in ${YT}`));
+            replace(YT, YT, MTW, () => {
+                console.log(c.brightCyan(`ℹ Process complete`));
+                console.timeEnd(c.brightMagenta('Time taken'));
+            });
+        });
+    } else {
+        console.info(c.brightGreen(`✅ Successfully replaced any ${BTP.find} to ${BTP.replace} in ${YTM}`));
+        replace(YTM, YTM, BTP, () => {
+            console.info(c.brightGreen(`✅ Successfully replaced any ${WTM.find} to ${WTM.replace} in ${YTM}`));
+            replace(YTM, YTM, WTM, () => {
+                console.info(c.brightGreen(`✅ Successfully replaced any ${STN.find} to ${STN.replace} in ${YTM}`));
+                replace(YTM, YTM, STN, () => {
+                    console.info(c.brightGreen(`✅ Successfully duplicated ${YTM} to ${YT}`));
+                    duplicate(YTM, YT, () => {
+                        console.info(c.brightGreen(`✅ Successfully replaced any ${MTW.find} to ${MTW.replace} in ${YT}`));
+                        replace(YT, YT, MTW, () => {
+                            console.log(c.brightCyan(`ℹ Process complete`));
                         });
                     });
                 });
-            });
-            found = true;
-        }
-    })
-
-    if (!found) {
-        console.info(c.bold.brightBlue(`ℹ No unwanted strings found in ${YTM}, now duplicating and replacing to ${YT}`));
-        duplicate(YTM, YT, () => {
-            console.log(c.bold.brightGreen(`✅ Successfully duplicated ${YTM} to ${YT}`));
-            replace(YT, YT, MTW, () => {
-                console.log(c.bold.brightGreen(`✅ Successfully replaced any ${MTW.find} to ${MTW.replace} in ${YT}`));
-                console.info(c.bold.brightBlue(`ℹ Process complete`));
             });
         });
     }
