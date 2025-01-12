bb8924a3e08b424810c47b8c64422d8ee0387048
Venti1707 <nickohse3@gmail.com>
2025-01-12 21:57:23 +0800

    Create Remove_All_Hyperlinks.js

---
+++ Bookmarklets/Minified/Remove_All_Hyperlinks.js
@@ -0,0 +1,1 @@
+javascript:(()=>{document.querySelectorAll('a').forEach(function(link){link.removeAttribute('href');link.style.color='black';link.style.pointerEvents='none';link.style.fontStyle='normal'})})()
