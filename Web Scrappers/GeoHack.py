bc330123032f63c992edd016521d21565860fe8e
nic <97864034+Venti1707@users.noreply.github.com>
2025-05-22 21:09:39 +0800

    Update GeoHack.py

--- Web Scrappers/GeoHack.py
+++ Web Scrappers/GeoHack.py
@@ -29,8 +29,7 @@ def scrapeGeoHack(url):
     return data
 
 # Example usage
-# url = "https://geohack.toolforge.org/geohack.php?pagename=Singapore_Institute_of_Technology&params=1_17_25_N_103_50_58_E_region:SG_type:edu"
-url = "https://geohack.toolforge.org/geohack.php?language=ja&pagename=%E5%80%89%E6%95%B7%E8%B2%A8%E7%89%A9%E3%82%BF%E3%83%BC%E3%83%9F%E3%83%8A%E3%83%AB%E9%A7%85&params=34_31_19.6_N_133_43_25.9_E_type:railwaystation_region:JP-33"
+url = "https://geohack.toolforge.org/geohack.php?pagename=Singapore_Institute_of_Technology&params=1_17_25_N_103_50_58_E_region:SG_type:edu"
 try:
     scraped_data = scrapeGeoHack(url)
     print("✅ Scraped data", scraped_data)
